238d05feaa04f53ebbd4b12613c5e4b35dfc39be
RokasVuicekas <78362625+RokasVuicekas@users.noreply.github.com>
2021-07-05 12:35:52 +0300

    phpRokasV

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to E-Banking System!</h1>
+            </div>
+            </div>
+</header>
+
+
+
+<!-- <div class="page-header row no-gutters py-4">
+            <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+                <span class="text-uppercase page-subtitle">Dashboard</span>
+                <h3 class="page-title">Blog Overview</h3>
+            </div>
+</div> -->
